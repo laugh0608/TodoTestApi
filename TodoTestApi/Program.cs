3fd8de122a860468d43bec224c50f6816943e335
laugh0608 <laugh0608@foxmail.com>
2025-04-07 11:32:06 +0800

    添加并注册数据库上下文

--- TodoTestApi/Program.cs
+++ TodoTestApi/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TodoTestApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// 将数据库上下文添加到 DI 容器，并指定数据库上下文将使用内存中数据库
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
